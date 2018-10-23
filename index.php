d373d8f36b47dd3680383f70cf50e0ed4aec166f
abbdlaziz <43935016+abbdlaziz@users.noreply.github.com>
2018-10-23 17:54:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,3 +7,57 @@ if(!isset($_SESSION['UserData']['Username'])){
 ?>
 
 Selamat anda berhasil login!. <a href="logout.php">Click here</a> to Logout.
+
+<html>
+
+<head>
+ <title>BIODATAKU</title>
+
+<body>
+ <center>
+ <font size="25px">
+ BIODATAKU
+ </font>
+ </center>
+ 
+ <table>
+ <tr>
+ <td>Nama :</td>
+ <td>Pattimura (Thomas Matulessy)</td>
+ </tr>
+ <tr>
+ <td>Tempat/Tanggal Lahir :</td>
+ <td>Maluku, 8 Juni 1783</td>
+ </tr>
+ <tr>
+ <td>Agama :</td>
+ <td>Islam</td>
+ </tr>
+ <tr>
+ <td>Hobby :</td>
+ <td>Pahlawan</td>
+ </tr>
+ <tr>
+ <td>Biografi</td>
+ <td>Pattimura lahir pada tanggal 8 Juni 1783 dari ayah Frans Matulesi dengan Ibu Fransina Silahoi. Munurut M. Sapidja ( penulis buku sejarah pemerintahan pertama) mengatakan bahwa “pahlawan Pattimura tergolong turunan bangsawan dan berasal dari Nusa Ina (Seram). Ayah beliau yang bernama Antoni Mattulessy adalah anak dari Kasimiliali Pattimura Mattulessy. Yang terakhir ini adalah putra raja Sahulau. Sahulau merupakan nama orang di negeri yang terletak dalam sebuah teluk di Seram Selatan"
+
+Ia adalah pahlawan yang berjuang untuk Maluku melawan VOC Belanda. Sebelumnya Pattimura adalah mantan sersan di militer Inggris. pada tahun 1816 Inggris bertekuk lutut kepda belanda. Kedatangan kembali kolonial Belanda pada tahun 1817 mendapat tantangan keras dari rakyat. Hal ini disebabkan karena kondisi politik, ekonomi, dan hubungan kemasyarakatan yang buruk selama dua abad. Rakyat Maluku akhirnya bangkit mengangkat senjata di bawah pimpinan Kapitan Pattimura.
+
+Sebagai panglima perang, Kapitan Pattimura mengatur strategi perang bersama pembantunya. Sebagai pemimpin dia berhasil mengoordinir raja-raja dan patih dalam melaksanakan kegiatan pemerintahan, memimpin rakyat, mengatur pendidikan, menyediakan pangan dan membangun benteng-benteng pertahanan. Dalam perjuangan menentang Belanda ia juga menggalang persatuan dengan kerajaan Ternate dan Tidore, raja-raja di Bali, Sulawesi dan Jawa. Perang Pattimura hanya dapat dihentikan dengan politik adu domba, tipu muslihat dan bumi hangus oleh Belanda.
+
+Di Saparua, dia dipilih oleh rakyat untuk memimpin perlawanan. Untuk itu, ia pun dinobatkan bergelar Kapitan Pattimura. Pada tanggal 16 Mei 1817, suatu pertempuran yang luar biasa terjadi. Rakyat Saparua di bawah kepemimpinan Kapitan Pattimura tersebut berhasil merebut benteng Duurstede. Tentara Belanda yang ada dalam benteng itu semuanya tewas, termasuk Residen Van den Berg.
+
+Pasukan Belanda yang dikirim kemudian untuk merebut kembali benteng itu juga dihancurkan pasukan Kapitan Pattimura. Alhasil, selama tiga bulan benteng tersebut berhasil dikuasai pasukan Kapitan Patimura. Namun, Belanda tidak mau menyerahkan begitu saja benteng itu. Belanda kemudian melakukan operasi besar-besaran dengan mengerahkan pasukan yang lebih banyak dilengkapi dengan persenjataan yang lebih modern. Pasukan Pattimura akhirnya kewalahan dan terpukul mundur.
+
+Di sebuah rumah di Siri Sori, Kapitan Pattimura berhasil ditangkap pasukan Belanda. Bersama beberapa anggota pasukannya, dia dibawa ke Ambon. Di sana beberapa kali dia dibujuk agar bersedia bekerjasama dengan pemerintah Belanda namun selalu ditolaknya.
+ 
+Para tokoh pejuang akhirnya dapat ditangkap dan mengakhiri pengabdiannya di tiang gantungan pada tanggal 16 Desember 1817 di kota Ambon. Atas kegigihannya memperjuangkan kemerdekaan, Kapitan Pattimura dikukuhkan sebagai “Pahlawan Perjuangan Kemerdekaan” oleh pemerintah Republik Indonesia.
+
+Riset Dan Analisa Oleh Nur Laila</td>
+ </tr>
+ <tr>
+
+</body>
+</head>
+
+</html>
